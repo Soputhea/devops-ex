cf47aeeb5a41a9c05cffe56b0dea4418c0cab570
Soputhea <puthealy123@gmail.com>
2024-08-09 08:50:38 +0700

    Change to Leaning cicd

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.status(200).send('Hello, World, Learning CI/CD !');
+  res.status(200).send('Learning CI/CD!');
 });
 
 app.get('/user/:id', (req, res) => {
